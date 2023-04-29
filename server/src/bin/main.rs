906af4b6ed7377435248e0dbe0cd72b153950f1e
Aaron Lee <aaron@dylibso.com>
2023-04-29 16:32:14 -0500

    pull the plugin manager into the library

---
+++ server/src/bin/main.rs
@@ -0,0 +1,50 @@
+use anyhow::Result;
+use clap::Parser;
+use rumqttd::{Broker, Config};
+use server::start_plugins;
+
+use std::{path::PathBuf, thread};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    plugin_file: Option<PathBuf>,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let builder = tracing_subscriber::fmt()
+        .pretty()
+        .with_line_number(false)
+        .with_file(false)
+        .with_thread_ids(false)
+        .with_thread_names(false);
+
+    builder
+        .try_init()
+        .expect("initialized subscriber succesfully");
+
+    // Config file is hardcoded to the current directory
+    let config = config::Config::builder()
+        .add_source(config::File::with_name("rumqttd.toml"))
+        .build()
+        .unwrap();
+
+    let config: Config = config.try_deserialize().unwrap();
+
+    dbg!(&config);
+
+    let mut broker = Broker::new(config);
+    let (link_tx, link_rx) = broker.link("singlenode").unwrap();
+
+    thread::spawn(move || {
+        broker.start().unwrap();
+    });
+
+    if let Some(plugin_filename) = args.plugin_file {
+        start_plugins(plugin_filename, link_tx, link_rx)?;
+    }
+    Ok(())
+}
---
+++ server/src/lib.rs
@@ -0,0 +1,41 @@
+use std::path::PathBuf;
+
+use anyhow::Result;
+use extism::{Context, Function, Plugin};
+use rumqttd::{
+    local::{LinkRx, LinkTx},
+    Notification,
+};
+
+pub fn start_plugins(plugin_file: PathBuf, mut link_tx: LinkTx, mut link_rx: LinkRx) -> Result<()> {
+    // load the plugin
+    let wasm = std::fs::read(plugin_file)?;
+    let context = Context::new();
+    let functions = std::iter::empty::<&Function>();
+    let mut _plugin = Plugin::new(&context, wasm, functions, false).unwrap();
+
+    link_tx.subscribe("#").unwrap();
+
+    let mut count = 0;
+    loop {
+        let notification = match link_rx.recv().unwrap() {
+            Some(v) => v,
+            None => continue,
+        };
+
+        match notification {
+            Notification::Forward(forward) => {
+                count += 1;
+                println!(
+                    "Topic = {:?}, Count = {}, Payload = {} bytes",
+                    forward.publish.topic,
+                    count,
+                    forward.publish.payload.len()
+                );
+            }
+            v => {
+                println!("{v:?}");
+            }
+        }
+    }
+}
