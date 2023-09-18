3bbc0a27d3f9187e02567d9ae855be052dd1af52
Aaron Lee <alee@aaronosaur.us>
2023-09-17 20:39:48 -0600

    parse json in plugin

--- double-plugin/src/lib.rs
+++ double-plugin/src/lib.rs
@@ -1,10 +1,28 @@
 use extism_pdk::*;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct InData {
+    data: f64,
+}
 
 #[plugin_fn]
-pub fn double(input: String) -> FnResult<String> {
-    if let Ok(num) = input.parse::<f64>() {
-        Ok(format!("{}", num * 2.0))
-    } else {
-        Ok("couldn't parse input".to_string())
+pub fn handle(input: String) -> FnResult<String> {
+    run_handle(input)
+}
+
+fn run_handle(input: String) -> FnResult<String> {
+    let data:InData = serde_json::from_str(&input).unwrap();
+    Ok((data.data * 2.0).to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_parses() {
+        let test = run_handle(r#"{"data": 2 }"#.to_owned()).unwrap();
+        assert_eq!("4", test);
     }
 }
