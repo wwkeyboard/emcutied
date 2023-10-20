fe8740d8f08d2253f054b9164202db6407a6e2e2
Aaron Lee <alee@aaronosaur.us>
2023-10-20 14:58:28 -0500

    new plugin that doesn't use host function

--- double-plugin/src/lib.rs
+++ double-plugin/src/lib.rs
@@ -1,12 +1,7 @@
 use extism_pdk::*;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-// The host function
-extern "C" {
-    fn emit(topic: u64, data: u64) -> ();
-}
-
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct InData {
     data: f64,
 }
@@ -19,16 +14,12 @@ pub fn handle(input: String) -> FnResult<String> {
 fn run_handle(input: String) -> FnResult<String> {
     let data: InData = serde_json::from_str(&input).unwrap();
 
-    let payload = format!("{{\"data\": {}}}", data.data * 2.0);
-    let payload_offset = Memory::from_bytes(&payload).offset as u64;
+    let payload = InData {
+        data: data.data * 2.0,
+    };
 
-    let topic_offset = Memory::from_bytes("demo/doubler").offset as u64;
-
-    unsafe {
-        emit(topic_offset, payload_offset);
-    }
-
-    Ok(payload)
+    let response = serde_json::to_string(&payload).unwrap();
+    Ok(response)
 }
 
 #[cfg(test)]
